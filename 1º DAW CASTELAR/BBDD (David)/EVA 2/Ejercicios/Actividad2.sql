fba30ca7d7b68423b2a728bc32a8fbc6864ee1da
Yago <catalanoyagocolegio15@gmail.com>
2023-12-13 21:28:15 +0100

    asd

--- 1º DAW CASTELAR/BBDD (David)/EVA 2/Ejercicios/Actividad2.sql
+++ 1º DAW CASTELAR/BBDD (David)/EVA 2/Ejercicios/Actividad2.sql
@@ -1,26 +1,35 @@
 CREATE TABLE Libro (
-    autor CHAR,
-    titulo CHAR,
-    indice NUMBER,
-    editor CHAR, 
-    clase  CONSTRAINT clase REFERENCES Clase_nueva(clave),
-    prestado CHAR
+    autor VARCHAR(20),
+    titulo VARCHAR2(10),
+    editor VARCHAR2(20), 
+    clase  CONSTRAINT clasee REFERENCES Clase(clave),
+    prestado NUMBER(1) DEFAULT 1
 );
 CREATE TABLE Usuario (
-    secuencia CHAR PRIMARY KEY,
-    nombre CHAR,
-    sexo CHAR(1),
-    direccion CHAR,
-    fecha_ingreso DATE
+    secuencia number PRIMARY KEY,
+    nombre VARCHAR2(20),
+    direccion VARCHAR(10),
+    fecha_ingreso DATE,
+    clase NUMBER CONSTRAINT clase REFERENCES Clase(clave)
 );
-CREATE TABLE Clase_nueva (
+CREATE TABLE Clase(
     clave INT PRIMARY KEY ,
     planta INT,
     tiempo_de_prestamo DATE NOT NULL
 );
 CREATE TABLE Prestamo (
-    codigo CONSTRAINT fk_codigo REFERENCES Libro (codigo),
-    secuencia CONSTRAINT fk_secuencia REFERENCES Usuarios(secuencia),
-    cuantia CHAR,
-    fecha_inicio DATE
-);
+    codigo NUMBER,
+    secuencia NUMBER CONSTRAINT ct_fk_secuencia REFERENCES Usuario(secuencia),
+    fecha_inicio DATE,
+    CONSTRAINT ct_pk_codsecfec PRIMARY KEY (codigo,secuencia,fecha_inicio),
+    CONSTRAINT ct_fk_cod_pres FOREIGN KEY (codigo) REFERENCES libro(codigo)
+    );
+ALTER TABLE usuario ADD sexo CHAR(1);
+DESCRIBE usuario;
+ALTER TABLE libro ADD indice NUMBER;
+ALTER TABLE uduario MODIFY nombre NOT NULL;
+RENAME clase TO clase_nueva;
+ALTER TABLE prestamo ADD cuantia NUMBER(*,2);
+SELECT * FROM all_contraints WHERE table_name='USUARIO';
+ALTER TABLE usuario DROP PRIMARY KEY CASCADE;
+DROP TABLE Usuario;
---
+++ 1º DAW CASTELAR/PGRMC (Pablo)/EVA 2/Aceptaelreto/aceptaelreto/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+
+    }
+    public static void acarreo(){
+        Scanner sc = new Scanner(System.in);
+        int numeros=sc.nextInt();
+        String cifras = numeros.length();
+        for (int i=0; i<numeros.length; i++){
+
+        }
+
+
+    }
+}
